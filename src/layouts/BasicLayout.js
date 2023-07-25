52acaee1b9ed539a6536bf87f562dbd8a333f63a
黄立根 <476421978@qq.com>
2023-07-25 17:39:45 +0800

    不同的全局layout ，umi只支持一个 可判断location.pathname显示 或者找个ant-design-pro@2.1.1模版支持

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import SimpleLayout from "./SimpleLayout"
 class BasicLayout extends React.PureComponent {
   constructor(props) {
     super(props)
@@ -8,6 +8,13 @@ class BasicLayout extends React.PureComponent {
 
   render() {
     const { children } = this.props
+
+    // 不同的全局 layout
+    if (this.props.location.pathname === "/user/login") {
+      // return <div>不同布局</div>
+      return <SimpleLayout></SimpleLayout>
+    }
+
     return <div>{children}</div>
   }
 }
